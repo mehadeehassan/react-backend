b437ac6a471acc84a3245f444beb59b6ffe39102
Mehadee Hassan <mehedi19999@gmail.com>
2026-06-28 02:14:32 +0600

    database fixed

--- Config/database.js
+++ Config/database.js
@@ -11,12 +11,12 @@ const database = process.env.DATABASE_URL
       },
     })
   : new Sequelize(
-      process.env.DB_NAME || "shops",
-      process.env.DB_USER || "root",
-      process.env.DB_PASSWORD || "",
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
       {
-        host: process.env.DB_HOST || "localhost",
-        port: Number(process.env.DB_PORT) || 3306,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
         dialect: "mysql",
       }
     );
